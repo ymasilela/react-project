5288f9f78e3bb0c80aede7d8ec29e2a647722d20
ymasilela <ymasilela1992@gmail.com>
2022-04-29 05:23:32 +0200

    Update useOfflineStorage.ts

--- src/hooks/useOfflineStorage.ts
+++ src/hooks/useOfflineStorage.ts
@@ -6,5 +6,37 @@ interface IUseOfflineStorage {
 export const useOfflineStorage: () => IUseOfflineStorage = () => {
 
   return {
+	  <div className="App">
+         <h1>Name of the user:</h1>
+         <input
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+         />
+         <h1>Project of the user:</h1>
+         <input
+            type="Project"
+            placeholder="Project"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+         />
+         <div>
+            <button onClick={handle}>Done</button>
+         </div>
+         {localStorage.getItem('Name') && (
+            <div>
+               Name: <p>{localStorage.getItem('Name')}</p>
+            </div>
+         )}
+         {localStorage.getItem('Project') && (
+            <div>
+               Project: <p>{localStorage.getItem('Projecct')}</p>
+            </div>
+         )}
+         <div>
+            <button onClick={remove}>Remove</button>
+         </div>
+      </div> 
   }
 }
+
