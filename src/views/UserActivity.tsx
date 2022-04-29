0a30a64e3073dc671f47ee00f1336c7cbe0e5429
ymasilela <ymasilela1992@gmail.com>
2022-04-29 05:28:59 +0200

    Update UserActivity.tsx

--- src/views/UserActivity.tsx
+++ src/views/UserActivity.tsx
@@ -1,7 +1,10 @@
 
 const UserActivityView = () => {
 
-  return <></>
-}
+ return (
+    <div className="SearchInput__Container">
+      <input className={'SearchInput'} type="text" placeholder={' Type github user name'} value={searchText} onChange={(event: React.ChangeEvent<HTMLInputElement>) => onSearchInputChange(event.target.value as string)}/>
+    </div>
+  );
 
 export default UserActivityView;
