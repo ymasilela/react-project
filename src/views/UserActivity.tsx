81e15c1c41ea2979d36ce7457fe997b3ae0cb6b9
ymasilela <ymasilela1992@gmail.com>
2022-04-29 05:31:43 +0200

    Update UserActivity.tsx

--- src/views/UserActivity.tsx
+++ src/views/UserActivity.tsx
@@ -1,9 +1,22 @@
-
+import React,{FC} from 'react';
+import UserDetail from '../UserActivity/UserActivity';
+interface UserResultListProp{
+   users: Array<UserSearch>,
+   noOfApiCall: number
+}
 const UserActivityView = () => {
 
- return (
-    <div className="SearchInput__Container">
-      <input className={'SearchInput'} type="text" placeholder={' Type github user name'} value={searchText} onChange={(event: React.ChangeEvent<HTMLInputElement>) => onSearchInputChange(event.target.value as string)}/>
+const userList = users.map((user,index) =>
+     <UserActivity user={user} key={index}/>
+    );
+  if(users.length <= 0) return <div>
+    <h3>User Results : (called api {noOfApiCall} times)</h3>
+    <p>No Users!</p>
+  </div>
+  return (
+    <div className="UserResultList__Container">
+        <h3>User Results : (called api {noOfApiCall} times)</h3>
+        {userList}
     </div>
   );
 
